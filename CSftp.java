d05c106a8bf864f2c8ad6684d04a06c09c399f8e
zafercavdar <zafercavdar@yahoo.com>
2018-01-13 22:45:33 -0800

    read host and port

--- CSftp.java
+++ CSftp.java
@@ -1,13 +1,25 @@
-package a1_g0o1b_y2m1b;
+import org.apache.commons.net.ftp.FTPClient;
 
-public class Main {
+public class CSftp {
 
 	public static void main(String[] args) {
-		String host = args[0];
-		int port = 21;
-		if (args.length > 1) {
-			port = Integer.parseInt(args[1]);
-		}
-		System.out.println("Trying connect to host " + host + " at port: " + port);
+    if (args.length ==  0) {
+      System.out.println("Insufficient command line arguments. Exiting ...");
+      System.exit(0);
+    } else {
+      String host = args[0];
+      int port = 21;
+      if (args.length > 1) {
+        port = Integer.parseInt(args[1]);
+      }
+      System.out.println("Trying connect to host " + host + " at port: " + port);
+      FTPClient ftpClient = new FTPClient();
+      try {
+        ftpClient.connect(host, port);
+      } catch (Exception e) {
+        e.printStackTrace(System.out);
+      }
+      //System.out.println(connectionResponse);
+    }
 	}
 }
